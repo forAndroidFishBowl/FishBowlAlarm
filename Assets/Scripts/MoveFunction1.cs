ed9364410051ca9ae52ba018485e1ef37f191c51
ater123zz00 <ater123zz00@gmail.com>
2017-12-04 13:36:46 +0800

    修正
    魚 轉向
    Range.random 變數
    邊界處理

--- Assets/Scripts/MoveFunction1.cs
+++ Assets/Scripts/MoveFunction1.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveFunction1 : MonoBehaviour {
+
+    float MaxLeftSide = 330f;
+    float MaxRightSide = 820f;
+    float MaxTopSide = 550f;
+    float MaxBottomSide = 0f;
+
     float time = 0f;
     int timeCounter = 0;
     int direction = 0;
@@ -12,7 +18,32 @@ public class MoveFunction1 : MonoBehaviour {
     {
         direction = getNextDirection();
     }
+    void checkSide(Vector3 Fish_position)//邊界處理
+    {
+        int OutSide = 0;
+        if (Fish_position.x > MaxRightSide) OutSide = 1;
+        else if (Fish_position.x < MaxLeftSide) OutSide = 2;
+        else if (Fish_position.y < MaxBottomSide) OutSide = 3;
+        else if (Fish_position.y > MaxTopSide) OutSide = 4;
 
+        switch (OutSide)
+        {
+            case 1:
+                gameObject.transform.position = new Vector3(MaxLeftSide, gameObject.transform.position.y, 0);
+                break;
+            case 2:
+                gameObject.transform.position = new Vector3(MaxRightSide, gameObject.transform.position.y, 0);
+                break;
+            case 3:
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, MaxTopSide, 0);
+                break;
+            case 4:
+                gameObject.transform.position = new Vector3(gameObject.transform.position.x, MaxBottomSide, 0);
+                break;
+            default:
+                break;
+        }
+    }
     int getNextDirection()
     {
         return prandom.Next(8);
@@ -21,37 +52,19 @@ public class MoveFunction1 : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        /*
-        time += Time.deltaTime;
-        if (time > 1f)
-        {
-             int fishmove=0;
-            if(time > 1.5f)fishmove = Random.Range(1, 3);
 
-            switch (fishmove)
-            {
-                case 1:
-                        gameObject.transform.position -= new Vector3(0.1f, 0, 0);
-                    break;
-                case 2:
-                    gameObject.transform.position += new Vector3(0.1f, 0, 0);
-                    break;
-                default:
-                    break;
-            }
-           if (time > 3f) time = 0.5f;
-        }
-        */
-
+        checkSide(gameObject.transform.position);
         if (time < 3f)
         {
             time += Time.deltaTime;
             switch (direction)
             {
                 case 3:
+                    gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
                     gameObject.transform.position += new Vector3(0.1f, 0, 0);
                     break;
                 case 2:
+                    gameObject.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
                     gameObject.transform.position += new Vector3(-0.1f, 0, 0);
                     break;
                 case 4:
@@ -61,15 +74,19 @@ public class MoveFunction1 : MonoBehaviour {
                     gameObject.transform.position += new Vector3(0, -0.1f, 0);
                     break;
                 case 6:
+                    gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
                     gameObject.transform.position += new Vector3(0.1f, 0.1f, 0);
                     break;
                 case 7:
+                    gameObject.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
                     gameObject.transform.position += new Vector3(-0.1f, 0.1f, 0);
                     break;
                 case 8:
+                    gameObject.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
                     gameObject.transform.position += new Vector3(0.1f, -0.1f, 0);
                     break;
                 case 5:
+                    gameObject.transform.rotation = Quaternion.Euler(0f, 180f, 0f);
                     gameObject.transform.position += new Vector3(-0.1f, -0.1f, 0);
                     break;
             }
@@ -77,7 +94,7 @@ public class MoveFunction1 : MonoBehaviour {
         else
         {
             time = 0f;
-            direction = getNextDirection();
+            direction = Random.Range(1, 8);
         }
 
 
