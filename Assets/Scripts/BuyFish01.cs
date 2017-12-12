52ba69828d5afd26121c833c23e45b5943612fac
apulu <30386790+apulu@users.noreply.github.com>
2017-12-12 13:14:41 +0800

    shop

---
+++ Assets/Scripts/BuyFish01.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuyFish01 : MonoBehaviour {
+
+    public GameObject FishUBuy;
+
+    public void Active()
+    {
+        FishUBuy.SetActive(true);
+    }
+
+}
