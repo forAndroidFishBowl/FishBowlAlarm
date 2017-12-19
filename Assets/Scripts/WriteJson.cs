ebd430ac413965de52a1537cc8ec0dc94cbb5b97
apulu <30386790+apulu@users.noreply.github.com>
2017-12-19 15:02:29 +0800

    successfully make the fish disapeared

---
+++ Assets/Scripts/WriteJson.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using LitJson;
+
+public class WriteJson : MonoBehaviour {
+    private string jsonString;
+    private JsonData jsonData;
+
+    void Start()
+    {
+        jsonString = File.ReadAllText(Application.dataPath + "/Resource/Status.json");
+        jsonData = JsonMapper.ToObject(jsonString);
+        Debug.Log(jsonData["fish"][0]["enable"]);
+        Debug.Log(jsonData["fish"][1]["enable"]);
+        Debug.Log(jsonData["fish"][2]["enable"]);
+        Debug.Log(jsonData["fish"][3]["enable"]);
+
+    }
+}
+
+public class User
+{
+    public bool fish1;
+    public bool fish2;
+    public bool fish3;
+    public bool fish4;
+
+    public User(bool fish1,bool fish2,bool fish3,bool fish4)
+    {
+        this.fish1 = fish1;
+        this.fish2 = fish2;
+        this.fish3 = fish3;
+        this.fish4 = fish4;
+    }
+}
---
+++ Assets/Scripts/main_load.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using LitJson;
+
+public class main_load : MonoBehaviour {
+
+    private JsonData jsonData;
+    
+
+	// Use this for initialization
+	void Start () {
+        GameObject fish1 = GameObject.Find("Fish1");
+        GameObject fish2 = GameObject.Find("Fish2");
+        GameObject fish3 = GameObject.Find("Fish3");
+        GameObject fish4 = GameObject.Find("Fish4");
+        jsonData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/Resource/Status.json"));
+        Debug.Log((bool)jsonData["fish"][0]["enable"]);
+        fish1.GetComponent<Renderer>().enabled = (bool)jsonData["fish"][0]["enable"];
+        fish2.GetComponent<Renderer>().enabled = (bool)jsonData["fish"][1]["enable"];
+        fish3.GetComponent<Renderer>().enabled = (bool)jsonData["fish"][2]["enable"];
+        fish4.GetComponent<Renderer>().enabled = (bool)jsonData["fish"][3]["enable"];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
