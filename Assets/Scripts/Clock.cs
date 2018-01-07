8ac4c8a9ad9860fb46856ed60e1c0286109a498c
apulu <30386790+apulu@users.noreply.github.com>
2018-01-08 00:38:13 +0800

    updated

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -20,14 +20,14 @@ public class Clock : MonoBehaviour {
     {
         User user = JsonMapper.ToObject<User>(File.ReadAllText(Application.persistentDataPath + "/Status.json"));
 
-        Rect rect1 = new Rect(0.0f, 0.0f, 200.0f, 100.0f);
-        Rect rect2 = new Rect(200.0f, 0.0f, 200.0f,100.0f);
-        Rect rect3 = new Rect(0.0f, 100.0f, 30.0f, 30.0f);
+        Rect rect1 = new Rect(0.0f, 0.0f, 600.0f, 200.0f);
+        Rect rect2 = new Rect(600.0f, 0.0f, 600.0f,200.0f);
+        Rect rect3 = new Rect(0.0f, 200.0f, 90.0f, 90.0f);
 
         GUIStyle font = new GUIStyle();
         GUIStyle font2 = new GUIStyle();
-        font.fontSize = 60;
-        font2.fontSize = 20;
+        font.fontSize = 180;
+        font2.fontSize = 40;
 
         GUI.Label(rect1, nowTimeHour + ":" + nowTimeMinute, font);
 
@@ -35,13 +35,13 @@ public class Clock : MonoBehaviour {
         inputMinute = Regex.Replace(inputMinute, "[^0-9]", "");
  
         GUI.Label(rect3, "設定鬧鐘時間 ",font2);
-        inputHour = GUI.TextField(new Rect(130, 100, 30, 30), inputHour, 2);
-        GUI.Label(new Rect(160, 100, 30, 30), "  : ", font2);
-        inputMinute = GUI.TextField(new Rect(190, 100, 30, 30), inputMinute, 2);
-        GUI.Label(new Rect(0, 200, 200,100), "鬧鐘時間 " + saveTimeHour + " : " + saveTimeMinute,font2);
+        inputHour = GUI.TextField(new Rect(250, 200, 100, 50), inputHour, 2);
+        GUI.Label(new Rect(350, 200, 50, 50), "  : ", font2);
+        inputMinute = GUI.TextField(new Rect(400, 200, 100, 50), inputMinute, 2);
+        GUI.Label(new Rect(0, 300, 600,300), "鬧鐘時間 " + saveTimeHour + " : " + saveTimeMinute,font2);
      
-        GUI.Label(new Rect(400, 0, 150, 50), "金錢" + user.money.ToString(), font2);
-        if (GUI.Button(new Rect(50,150,50,50),"確認"))
+        GUI.Label(new Rect(1200, 0, 450, 150), "金錢" + user.money.ToString(), font2);
+        if (GUI.Button(new Rect(150,350,150,50),"確認"))
         {
             saveTimeHour = inputHour;
             saveTimeMinute = inputMinute;
